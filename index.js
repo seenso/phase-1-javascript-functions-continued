8f7c15d621924aad4ef4437de3cbe9f24916bf40
seenso <>
2021-11-01 13:15:03 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ function mondayWork(activity = 'go to the office') {
   return `This Monday, I will ${activity}.`;
 };
 
-function wrapAdjective() {};
+function wrapAdjective(ele) {
+  return function result(adj = 'special') {
+    return `You are ${ele}${adj}${ele}!`;
+  }
+};
