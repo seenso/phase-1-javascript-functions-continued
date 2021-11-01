cfab6289d46e99c9a24a341cfd9d5ff4f92aece2
seenso <>
2021-11-01 12:50:29 -0800

    passed test for saturdayFun()

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+function saturdayFun(activity) {
+  if (activity === undefined) {
+    activity = 'roller-skate';
+  }
+  return `This Saturday, I want to ${activity}!`;
+};
+
+function mondayWork() {};
+
+function wrapAdjective() {};
