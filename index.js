e5ac88d11d69b3fa883a525946bbc0aebcb49b0d
seenso <>
2021-11-01 13:01:56 -0800

    Passed test for mondayWork()

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function saturdayFun(activity = 'roller-skate') {
   return `This Saturday, I want to ${activity}!`;
 };
 
-function mondayWork() {};
+function mondayWork(activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`;
+};
 
 function wrapAdjective() {};
